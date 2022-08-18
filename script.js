e84d5b49268352d26ca6066e0cfecd8274719be3
Taliour <Ktaliour@gmail.com>
2022-08-18 16:17:44 +0300

    Take user input and proceed only if it chooses between rock, paper or scissors

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let choices = ["rock","paper","scissors"];
+let userChoice;
+let validChoice = false;
+while (validChoice == false){
+    userChoice = prompt("Choose between, rock, paper or scissors!");
+    userChoice = userChoice.toLowerCase();
+    
+    if (userChoice =="rock" ||userChoice == "paper" ||userChoice == "scissors") {
+        validChoice = true;
+    }
 
-let userChoice = prompt();
+}
 console.log(userChoice);
-
 function getComputerChoice(){
     let number = Math.floor(Math.random()*3);
     return choices[number];
