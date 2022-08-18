b499c1ac7a6635ad8cae06e7d16212c1c396934b
Taliour <Ktaliour@gmail.com>
2022-08-18 16:35:08 +0300

    Add function play game, that checks for who wins, add comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+//Global variables and arrays
 let choices = ["rock","paper","scissors"];
 let userChoice;
 let validChoice = false;
+//Take user input and allow only inputs for rock, paper or scissors
 while (validChoice == false){
     userChoice = prompt("Choose between, rock, paper or scissors!");
     userChoice = userChoice.toLowerCase();
@@ -10,9 +12,42 @@ while (validChoice == false){
     }
 
 }
-console.log(userChoice);
+//make computer take a guess
+console.log("You chose: " + userChoice);
 function getComputerChoice(){
     let number = Math.floor(Math.random()*3);
     return choices[number];
 }
-console.log(getComputerChoice())
+
+function playGame(playerSelection, computerSelection) {
+    let str = "";
+    if (playerSelection === computerSelection){
+        str = "It's a draw ! You both picked: " + playerSelection;
+    }
+    else if (playerSelection == "rock"){
+        if(computerSelection == "scissors"){
+            str = "You win, " + playerSelection + " beats " + computerSelection;
+        }
+        else {
+            str = "You lose, " + computerSelection + " beats " + playerSelection;
+        }
+    }
+    else if (playerSelection == "paper"){
+        if(computerSelection == "rock"){
+            str = "You win, " + playerSelection + " beats " + computerSelection;
+        }
+        else {
+            str = "You lose, " + computerSelection + " beats " + playerSelection;
+        }
+    }
+    else if (playerSelection == "scissors"){
+        if(computerSelection == "paper"){
+            str = "You win, " + playerSelection + " beats " + computerSelection;
+        }
+        else {
+            str = "You lose, " + computerSelection + " beats " + playerSelection;
+        }
+    }
+    return str;
+}
+console.log(playGame(userChoice,getComputerChoice()));
