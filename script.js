277156174984ebef166d04647785b9b8b09593ec
Taliour <Ktaliour@gmail.com>
2022-08-20 13:36:16 +0300

    Take player input through ui, decide winner, highligth player choice

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
+//Targeting Nodes with Selectors
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+const score = document.querySelector(".score");
+const roundWinner = document.querySelector(".explain");
+
 //Global variables and arrays
 let choices = ["rock","paper","scissors"];
 let userChoice;
-let validChoice = false;
+let computerChoice;
+let validChoice = true;
 let playerScore = 0;
 let computerScore = 0;
 
-//Take user input and allow only inputs for rock, paper or scissors
+//Take user input and play the game if not finished
 function getPlayerChoice(){
-    while (validChoice == false){
-        userChoice = prompt("Choose between, rock, paper or scissors!");
-        userChoice = userChoice.toLowerCase();
-        
-        if (userChoice =="rock" ||userChoice == "paper" ||userChoice == "scissors") {
-            validChoice = true;
+    paper.addEventListener("click",()=> {userChoice="paper"; if(validChoice == true) {game()};});
+    rock.addEventListener("click", () => {userChoice="rock"; if(validChoice == true) {game()};});
+    scissors.addEventListener("click", () => {userChoice="scissors"; if(validChoice == true) {game()};});
+
+}
+//Function that plays the game
+function game(){
+    
+    //announce round wiiner
+    roundWinner.textContent = decideWinner(userChoice,getComputerChoice());
+
+    //update the score
+    score.textContent = playerScore + "-" + computerScore;
+
+
+    // Check who won the best of 5 and announce winner
+    if (playerScore >= 3 || computerScore >= 3){
+        let str = "";
+        if (computerScore > playerScore){
+            str = "You lose :( : ";
         }
+        else if (computerScore < playerScore){
+            str = "You win :) : ";
+        }
+        else {
+            str = "it's a draw :/ : "
+        }
+        console.log(str + playerScore + "-" + computerScore);
+        validChoice = false;
     }
 }
 
-//Function that plays the game with guess
-function game(){
-    
-    getPlayerChoice();
-    console.log("You chose: " + userChoice);
-    console.log(decideWinner(userChoice,getComputerChoice()));
-}
-
-//make computer take a guess
+//make computer take a guess, assign computerChoice value and return that value
 function getComputerChoice(){
     let number = Math.floor(Math.random()*3);
+    computerChoice = choices[number];
     return choices[number];
 }
 
@@ -35,7 +58,7 @@ function getComputerChoice(){
 function decideWinner(playerSelection, computerSelection) {
     let str = "";
     if (playerSelection === computerSelection){
-        str = "It's a draw ! You both picked: " + playerSelection;
+        str = "It's a draw! You both picked " + playerSelection;
     }
     else if (playerSelection == "rock"){
         if(computerSelection == "scissors"){
@@ -70,21 +93,21 @@ function decideWinner(playerSelection, computerSelection) {
     return str;
 }
 
-//Loop through the game
-//for (let i = 0; i < 5; i++){
-    //game();
-   // validChoice = false;
-//}
+//styling border images when hovered
 
-// Check who won the best of 5 and announce winner
-let str = "";
-if (computerScore > playerScore){
-    str = "You lose :( : ";
-}
-else if (computerScore < playerScore){
-    str = "You win :) : ";
-}
-else {
-    str = "it's a draw :/ : "
-}
-console.log(str + playerScore + "-" + computerScore);
+rock.addEventListener("mouseover",()=> {rock.style.cssText = "border: solid #856e21 6px"});
+rock.addEventListener("mouseout",()=> {rock.style.cssText="border: solid black 4px"});
+
+paper.addEventListener("mouseover",()=> {paper.style.cssText = "border: solid #856e21 6px"});
+paper.addEventListener("mouseout",()=> {paper.style.cssText="border: solid black 4px"});
+
+scissors.addEventListener("mouseover",()=> {scissors.style.cssText = "border: solid #856e21 6px"});
+scissors.addEventListener("mouseout",()=> {scissors.style.cssText="border: solid black 4px"});
+
+
+getPlayerChoice();
+
+
+
+
+
